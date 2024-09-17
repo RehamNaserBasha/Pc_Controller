571c8bd020f718af581cd00807d5d7b0a2ec1f3e
RehamNaserBasha <148005486+RehamNaserBasha@users.noreply.github.com>
2024-09-17 22:36:01 +0300

    Update server.hpp

--- server.hpp
+++ server.hpp
@@ -1,29 +1,17 @@
-#ifndef SERVER_SOCKET_HPP
-#define SERVER_SOCKET_HPP
-#include <array>
-#include<iostream>
-#include <string.h>
-#include <cstring>
-#include <string>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#ifndef SERVER_HPP
+#define SERVER_HPP
 
-class ServerSocket{
-private:
-int server_IB;
-int client_IB;
-sockaddr_in sockaddr_accept;
-std::array<char, 5000>buffer; 
+#include <string>
+
+class ServerSocket {
 public:
-ServerSocket();
-~ServerSocket();
-void trim(std::string &str);
-void send_data_to_client(const std::string &message);
-void send(const std::string &message);
-std::string read();
-std::string receive_data_from_client();
+    ServerSocket();
+    ~ServerSocket();
+
+    void trim(std::string &str);
+    std::string read();
+    void send_message(int client_socket, const std::string& message);
 };
+
+
 #endif
